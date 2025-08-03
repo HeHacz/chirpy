4b360f9e6a141cb535865761035b7b9475f2fd41
hehacz <hehahiyourip@gmail.com>
2025-08-03 22:44:42 +0200

    added few api handlers

--- main.go
+++ main.go
@@ -2,19 +2,57 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"sync/atomic"
 	"time"
 )
 
-func handler(w http.ResponseWriter, req *http.Request) {
-	http.NotFound(w, req)
-	return
-	//fmt.Fprint(w,"404 Not Found")
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, req)
+	})
+}
+
+func handlerMetrics(cfg *apiConfig) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		hits := cfg.fileserverHits.Load()
+		io.WriteString(w, fmt.Sprintf("Hits: %d\n", hits))
+	}
+}
+
+func handlerReset(cfg *apiConfig) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		cfg.fileserverHits.Swap(0)
+	}
+}
+
+func handlerHealthCheck(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, "OK")
 }
 
 func main() {
+	const rootPath = "."
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", handler)
+	handler := http.FileServer(http.Dir(rootPath))
+	mux.Handle("/app/", http.StripPrefix("/app/", apiCfg.middlewareMetricsInc(handler)))
+	mux.HandleFunc("/healthz", handlerHealthCheck)
+	mux.Handle("/metrics", handlerMetrics(&apiCfg))
+	mux.Handle("/reset", handlerReset(&apiCfg))
 	server := http.Server{
 		Addr:              "127.0.0.1:8080",
 		Handler:           mux,
@@ -28,5 +66,4 @@ func main() {
 	if err := server.ListenAndServe(); err != nil {
 		fmt.Printf("Server error: %v", err)
 	}
-
 }
