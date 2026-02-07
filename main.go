38774cd7fc66429cc23a98502513624507a13eef
HeHacz <obarski.pawel@gmail.com>
2026-02-07 22:37:47 +0100

    added sql queries

--- main.go
+++ main.go
@@ -1,19 +1,34 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
 	"time"
+
+	"github.com/hehacz/chirpy/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
+	dbQueries      *database.Queries
 	fileserverHits atomic.Int32
 }
 
 func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("cant open database")
+	}
 	const rootPath = "."
 	apiCfg := apiConfig{
+		dbQueries:      database.New(db),
 		fileserverHits: atomic.Int32{},
 	}
 	mux := http.NewServeMux()
